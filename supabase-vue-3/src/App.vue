b55e40c83bc03eda26aec131bdfcc510d901bf68
Mandy <139280587+MandyyChen@users.noreply.github.com>
2024-05-09 10:10:25 -0400

    name and pee

--- supabase-vue-3/src/App.vue
+++ supabase-vue-3/src/App.vue
@@ -5,13 +5,12 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/CLICKER.png" class="logo" alt="Whalen" height="1800px"/>
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="ClickBaited" />
 </template>
 
 <style scoped>
