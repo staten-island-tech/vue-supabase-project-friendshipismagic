104d376d3228ab14a50b870c5e86641edf955aac
Mandy <139280587+MandyyChen@users.noreply.github.com>
2024-05-15 10:14:55 -0400

    avatar

--- supabase-vue-3/src/views/gameView.vue
+++ supabase-vue-3/src/views/gameView.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-
+        <img src="../assets/henriques.png" alt="punch">
+        <img src="../assets/colangelo.png" alt="punch">
+        <img src="../assets/logo.png" alt="punch">
     </div>
 </template>
 
