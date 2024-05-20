59c9dbf31d9fa9543cbf6129e8fe7b01e12fd7fa
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2024-05-20 10:13:32 -0400

    add count to metadata

--- supabase-vue-3/src/components/gameComp.vue
+++ supabase-vue-3/src/components/gameComp.vue
@@ -2,13 +2,27 @@
   <div>
     <h1 class="title">Game Page</h1>
   </div>
-  <img src="../assets/logo.png" @click="count++" />
+  <img src="../assets/logo.png" @click="clickCounter" />
   <p>Punches: {{ count }}</p>
 </template>
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
-const count = ref(0)
+import { supabase } from "../clients/supabase";
+
+
+async function clickCounter(){
+  const count = ref(0)
+  const localUser = await supabase.auth.getSession({
+options: {
+  data: {
+    clickCount:
+  }
+}
+  });
+
+}
+
 </script>
 
 <style scoped>
