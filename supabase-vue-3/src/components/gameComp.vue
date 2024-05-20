939a82ce463452745adad03b48eedd196460279f
Mandy <139280587+MandyyChen@users.noreply.github.com>
2024-05-20 10:02:41 -0400

    clickclack

--- supabase-vue-3/src/components/gameComp.vue
+++ supabase-vue-3/src/components/gameComp.vue
@@ -2,10 +2,13 @@
   <div>
     <h1 class="title">Game Page</h1>
   </div>
+  <img src="../assets/logo.png" @click="count++" />
+  <p>Punches: {{ count }}</p>
 </template>
 
 <script setup lang="ts">
-
+import { ref, computed } from 'vue'
+const count = ref(0)
 </script>
 
 <style scoped>
