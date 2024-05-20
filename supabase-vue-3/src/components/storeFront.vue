083b0ec5ee63c08f88caec9a3a78001109b6dc9a
Mandy <139280587+MandyyChen@users.noreply.github.com>
2024-05-20 10:11:50 -0400

    store

---
+++ supabase-vue-3/src/components/storeFront.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="cards-container">
+    <div class="card">
+        <h2> {{ Item.name }}</h2>
+        <h3> ${{ Item.price }}.00</h3>
+        <img :src="Item.img" alt="" /> 
+        <button @click="toggleButton(); purchasedCard()" :class="{ 'placed': buttonText === 'Purchased' }"> {{ buttonText }} </button>
+    </div>
+    </div>
+</template>
+
+<script setup> 
+
+</script>
