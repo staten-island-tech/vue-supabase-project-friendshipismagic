f548345ce83bc8b3618ecd209690765245f84f67
Melanie <144461109+MelanieHuangg@users.noreply.github.com>
2024-05-21 10:15:00 -0400

    trying cursor

--- supabase-vue-3/src/views/gameView.vue
+++ supabase-vue-3/src/views/gameView.vue
@@ -3,6 +3,8 @@
         <img src="../assets/henriques.png" alt="punch">
         <img src="../assets/colangelo.png" alt="punch">
         <img src="../assets/logo.png" alt="punch">
+        <img src="../assets/punch.png" alt="punch">
+        
 
 </div>
 <GameComp />
@@ -13,7 +15,6 @@
 const items = [
 {name: 'bob',
 price: 9.99,
-img: "../assets/student1.png"
 }
 
 ]
