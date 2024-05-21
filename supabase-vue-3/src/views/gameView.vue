f2daf08a90d85f20f7f18cbd13a1d2970bbb8ce7
Melanie <144461109+MelanieHuangg@users.noreply.github.com>
2024-05-21 09:38:43 -0400

    chsia

--- supabase-vue-3/src/views/gameView.vue
+++ supabase-vue-3/src/views/gameView.vue
@@ -3,12 +3,12 @@
         <img src="../assets/henriques.png" alt="punch">
         <img src="../assets/colangelo.png" alt="punch">
         <img src="../assets/logo.png" alt="punch">
+
 </div>
 <GameComp />
 </template>
 
 <script setup lang="ts">
-import GameComp from '../components/gameComp.vue';
 
 const items = [
 {name: 'bob',
