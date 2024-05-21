3ba25ec7a61b70f6b0c60bb2ed11006ec74c68c1
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2024-05-20 21:19:17 -0400

    merge from main

--- supabase-vue-3/src/views/AccountInfo.vue
+++ supabase-vue-3/src/views/AccountInfo.vue
@@ -1,13 +1,41 @@
 <template>
-    <div>
-        <h1>ACCOUNT INFO</h1>
+    <div class="about">
+        <h1 class="title">Account Information</h1>
+        <div class="accountInfoContainer">
+        <p id="account"> 
+
+            E-mail: {{ account.data.session.user.email}} <br>
+            User Name: {{ account.data.session.user.user_metadata.user_name}} <br>
+            Created At: {{ account.data.session.user.created_at }}
+        </p>
+        </div>
     </div>
 </template>
 
-<script>
+<script setup lang="ts">
+import { supabase } from '../clients/supabase';
+import { ref } from "vue";
+
+const account = ref();
+getSession();
+
+async function getSession(){
+    account.value = await supabase.auth.getSession();
+}
+
 
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.accountInfoContainer{
+    background-color: beige;
+    border-radius: 1rem;
+    border-color: #8aa956;
+}
+#account {
+    color: #8aa956;
+}
+.title{
+    text-decoration: underline;
+}
 </style>
