87bb33da6d68d14ef9c0e43bdd0e2b73aa32d425
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2024-05-21 09:53:38 -0400

    a

--- supabase-vue-3/src/components/gameComp.vue
+++ supabase-vue-3/src/components/gameComp.vue
@@ -3,24 +3,26 @@
     <h1 class="title">Game Page</h1>
   </div>
   <img src="../assets/logo.png" @click="clickCounter" />
-  <p>Punches: {{ count }}</p>
+  <p>Punches: {{ account.data.session.user.user_metadata.click_count }}</p>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed } from 'vue';
 import { supabase } from "../clients/supabase"
 const count = ref(0)
+const account = ref();
 
-async function clickCounter(){
-  count.value++
-    const { data, error } = await supabase.auth.updateUser(
-      options: {
-        data: {
-          click_count: count.value
-        }
-      }
-    )
+async function getSession(){
+    account.value = await supabase.auth.getSession();
+    console.log(account.value)
 }
+getSession();
+// async function clickCounter(){
+//   count.value++
+//     const { data, error } = await supabase.auth.updateUser(
+ 
+//     )
+// }
 
 </script>
 
