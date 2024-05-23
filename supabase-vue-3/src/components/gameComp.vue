5aafa9b9c2375c7d7ad33bed80137f3e655d5bd0
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2024-05-23 17:45:00 -0400

    may 23
    - took update function from supabase documentation
    - clicker_count shows up as "count.value" instead of an integer

--- supabase-vue-3/src/components/gameComp.vue
+++ supabase-vue-3/src/components/gameComp.vue
@@ -2,25 +2,78 @@
   <div>
     <h1 class="title">Game Page</h1>
   </div>
-  <img src="../assets/logo.png" @click="clickCounter" />
-  <p>Punches: {{ account.data.session.user.user_metadata.click_count }} </p>
+  <img src="../assets/logo.png" @click="updateClicks" />
+  <p>Punches:</p>
 </template>
 
-<script setup lang="ts">
-import { ref, computed, onBeforeMount } from 'vue';
-import { supabase } from "../clients/supabase"
+<script lang="ts">
+import { onMounted, ref, toRefs } from 'vue'
+import { supabase } from "../supabase"
 const count = ref(0)
-const account = ref('');
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-// async function getSession(){
-//     account.value = await supabase.auth.getSession();
-//     console.log(account.value.)
-// }
 
-// async function getCount(){
+// const props = defineProps(['session']);
+// const { session } = toRefs(props);
+const session = ref();
+getSession();
 
-// }
+async function getSession(){
+    session.value = await supabase.auth.getSession();
+}
 
+
+const loading = ref(true);
+const username = ref('');
+const email = ref('');
+const clicker_count = ref(0);
+
+onMounted(() => {
+  getProfile()
+})
+
+async function getProfile() {
+  try {
+    loading.value = true
+    const { user } = session.value
+  
+    const { data, error, status } = await supabase
+      .from('users')
+      .select(`username, email, clicker_count`)
+      .eq('id', user.id)
+      .single()
+
+    if (error && status !== 406) throw error
+
+    if (data) {
+      username.value = data.username
+      email.value = data.email
+      clicker_count.value = data.clicker_count
+    }
+  } catch (error) {
+    alert(error.message)
+  } finally {
+    loading.value = false
+  }
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+async function updateClicks() {
+  count.value++
+  try {
+    loading.value = true
+    const { user } = session.value
+
+    const updates = {
+      clicker_count: 'count.value',
+    }
+
+    const { error } = await supabase.from('users').upsert(updates)
+
+    if (error) throw error
+  } catch (error) {
+    alert(error.message)
+  } finally {
+    loading.value = false
+  }
+}
 
 
 </script>
--- supabase-vue-3/src/views/gameView.vue
+++ supabase-vue-3/src/views/gameView.vue
@@ -1,9 +1,9 @@
 <template>
 <GameComp />
 <div>
-        <h1 class="title">Game Page</h1>
+        <!-- <h1 class="title">Game Page</h1>
   <img src="../assets/logo.png" @click="clickCounter" />
-  <p>Punches: {{ count }}</p>
+  <p>Punches: {{ count }}</p> -->
 
         <img src="../assets/henriques.png" alt="punch">
         <img src="../assets/colangelo.png" alt="punch">
@@ -15,7 +15,29 @@
 
 </template>
 
-<script setup lang="ts">
+<script>
+import GameComp from '../components/gameComp.vue'
+import { onMounted, ref } from 'vue'
+import { supabase } from "../clients/supabase";
+
+const session = ref()
+export default {
+      components: {
+      GameComp
+      }
+};
+
+onMounted(() => {
+  supabase.auth.getSession().then(({ data }) => {
+    session.value = data.session
+  })
+
+  supabase.auth.onAuthStateChange((_, _session) => {
+    session.value = _session
+  })
+})
+
+
 const items = [
 {name: 'bob',
 price: 500,
