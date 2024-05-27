229d6bb79db82fbfcb17ec02c59094bac94a0910
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2024-05-27 11:57:27 -0400

    Update gameView.vue

--- supabase-vue-3/src/views/gameView.vue
+++ supabase-vue-3/src/views/gameView.vue
@@ -15,6 +15,49 @@
 </template>
 
 <script setup lang="ts">
+import { supabase } from '../clients/supabase';
+export default {
+  name: 'GameView',
+  components: { GameComp },
+
+    data(){
+        return {
+            loaded: false,
+            clicks: [],
+        };
+    },
+    mounted: function(){
+        this.fetchData();
+    },
+    methods: {
+        fetchData: async function(){
+         this.loaded = false
+
+            try{
+                const results = await fetch(
+                    'https://data.cityofnewyork.us/resource/jb7j-dtam.json'
+                );
+                const data = await results.json();
+                let LeadingCause = data.results;
+
+                this.malcount = data.filter(element => element.leading_cause.includes ('Malignant'))
+                this.respritorycount = data.filter(element => element.leading_cause.includes ('Respiratory'))
+                this.accexdrugcount = data.filter(element => element.leading_cause.includes ('Accidents'))
+                this.suicidecount = data.filter(element => element.leading_cause.includes ('Self-Harm'))
+                this.alzcount = data.filter(element => element.leading_cause.includes ('Alzheimer'))
+                this.flucount = data.filter(element => element.leading_cause.includes ('Influenza'))
+                this.livercount = data.filter(element => element.leading_cause.includes ('Liver'))
+                this.septiccount = data.filter(element => element.leading_cause.includes ('Septicemia'))
+          
+                
+                console.log(data);
+                this.loaded=true
+            } catch(error){
+                console.log(error);
+            }
+        }
+    }
+}
 const items = [
 {name: 'bob',
 price: 500,
