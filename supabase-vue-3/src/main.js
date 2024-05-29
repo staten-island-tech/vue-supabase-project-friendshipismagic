581209476b3fedaa4fff9b20e1fcdc048b591347
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2024-05-28 21:36:53 -0400

    pinia not working?

--- supabase-vue-3/src/main.js
+++ supabase-vue-3/src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { createPinia } from "pinia";
+import App from './App.vue';
+import './style.css';
 
-createApp(App).use(router).mount('#app')
+import router from './router';
+
+const app = createApp(App);
+
+app.use(createPinia());
+
+app.mount('#app');
