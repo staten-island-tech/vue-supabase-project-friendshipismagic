4cadfaceb609a5fc4446e9fe11dcae23a37325f9
Mandy <139280587+MandyyChen@users.noreply.github.com>
2024-05-29 09:48:20 -0400

    f

--- supabase-vue-3/src/main.js
+++ supabase-vue-3/src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(createPinia()).mount('#app')
 
 const pinia = createPinia()
 const app = createApp(App)
