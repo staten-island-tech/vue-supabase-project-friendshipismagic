821a33db99d1aeb146130e07ad2ea16c690a5366
Melanie <144461109+MelanieHuangg@users.noreply.github.com>
2024-05-30 10:13:25 -0400

    hold

--- supabase-vue-3/src/components/gameComp.vue
+++ supabase-vue-3/src/components/gameComp.vue
@@ -2,13 +2,26 @@
   <div>
     <h1 class="title">Game Page</h1>
   </div>
-  <img src="../assets/logo.png" @click="clickCounter" />
-  <p>Punches: {{ count }}</p>
+  <!-- <img src="../assets/logo.png" @click="clickCounter" />
+  <p>Punches: {{ count }}</p> -->
 </template>
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 import { supabase } from "../clients/supabase";
+import { defineStore } from 'pinia'
+
+
+export const useCounterStore = defineStore('counter', {
+  state: () => ({
+    count: 0,
+  }),
+  actions: {
+    increment() {
+      this.count++
+    },
+  },
+})
 
 
 
